5124010be0833189c38027159e7bdcfccce4f20a
Ibraheem Sajid <ibraheem.sajid@gmail.com>
2025-11-28 18:36:04 +0000

    potts

--- potts.jl
+++ potts.jl
@@ -2,13 +2,21 @@ using Symbolics
 
 @variables x
 
-n = 2
-T₀ = Matrix{typeof(x)}(undef, 2^n, 2^n)
-Ω = Iterators.product(Iterators.repeated((false, true), n)...)
+const n = 2
+const T₀ = Matrix{typeof(x)}(undef, 2^n, 2^n)
+const Ω = Iterators.product(Iterators.repeated((false, true), n)...)
 
 for (i, Ωᵢ) ∈ zip(1:2^n, Ω),
-    (j, Ωₒ) ∈ zip(1:2^n, Ω),
+    (j, Ωₒ) ∈ zip(1:2^n, Ω)
 
+    p = 0
 
+    for r ∈ 1:n
+        p += Int(Ωᵢ[r] == Ωₒ[r])
+        p += Int(Ωᵢ[r] == Ωᵢ[mod1(r, n)])
+    end
 
+    T₀[i, j] = x^p
 end
+
+display(T₀)
